ca0a138d589621583bd34c4f0c4f5c913df44844
Abdullah <abdullahoguk@users.noreply.github.com>
2015-02-07 20:36:52 +0200

    Create LinkedListIterator.java

---
+++ LinkedListIterator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class LinkedListIterator<E> implements Iterator<E>
+{
+  private Node<E> nextNode;
+
+  public LinkedListIterator(Node<E> head)
+  {
+	 nextNode = head;
+  }
+
+  public boolean hasNext()
+  {
+	return nextNode != null;
+  }
+
+  public E next()
+  {
+	 if (!hasNext()) throw new NoSuchElementException();
+	 E res = nextNode.getElement();
+	 nextNode = nextNode.getNext();
+	 return res;
+  }
+
+  public void remove() { throw new UnsupportedOperationException(); }
+}
